cf606dbb61682f9d2150343f434d5673ce229088
Yssaky Assad <yssakyassad@gmail.com>
2024-05-23 17:56:13 -0300

    Projeto React, Vite iniciado

---
+++ JetFlix/src/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <NavBar />
+      <h2>Movies Lib</h2>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App;
